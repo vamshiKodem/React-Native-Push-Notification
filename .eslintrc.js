4f93d80e5351579d161e7c805338cf3008256f19
vamshiKodem <vamshikrishna.kodem96@gmail.com>
2024-03-09 12:40:08 +0530

    completed the setup for push notification

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-native/no-inline-styles': 0,
+  },
 };
